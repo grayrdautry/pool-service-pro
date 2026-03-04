dd6589fdb38a3f12c7bfa8946061d0c638a9e8be
grayrdautry <gray.rd.autry@gmail.com>
2026-03-03 21:48:58 -0500

    Update service-worker.js V3

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,11 @@
-const CACHE_NAME = "pool-pro-cache-v1";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/styles.css",
-        "/app.js"
-      ]);
-    })
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting(); 
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      for (let name of names) caches.delete(name);
+    }).then(() => self.clients.claim())
   );
 });
